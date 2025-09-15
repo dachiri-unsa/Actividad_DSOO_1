9b72faf6223d489c086365a4a2041b10c7c313cc
Xdaniel <dachiri@unsa.edu.pe>
2025-09-14 20:19:12 -0500

    editado

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,7 +1,14 @@
 public class Ejercicio {
     public static void main(String[]args){
+<<<<<<< HEAD
         System.out.println("Esta es una prueba");
         System.out.println("RUBIAS");
         System.out.println("hola");
+=======
+        while (true){
+            System.out.println("Bienvenido a Sistema de viajes");
+            break;
+        }
+>>>>>>> 3454619 (Bienvenida)
     }
 }
