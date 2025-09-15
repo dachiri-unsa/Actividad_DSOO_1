06bca89c830262a68e99735dcfd2d0ea2eab9806
Xdaniel <dachiri@unsa.edu.pe>
2025-09-14 22:28:23 -0500

    Avance de el chofer

--- Chofer.java
+++ Chofer.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Chofer {
     private String nombre,licencia;
-    private int edad, Experiencia;
+    private int edad, experiencia;
+
+    public Chofer() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar la placa del vehiculo: ");
+        nombre = sc.nextLine();
+        System.out.println("Ingresa la capacidad del vehiculo: ");
+        edad = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingresar el tipo de combustible del vehiculo: ");
+        licencia = sc.nextLine();
+        System.out.println("Ingresa el consumo de combustible por kilometro del vehiculo: ");
+        experiencia = sc.nextInt();
+        sc.nextLine();
+    }
+
     public void setNombre (String nom) {
         nombre=nom;
     }
@@ -20,9 +37,9 @@ public class Chofer {
         return edad;
     }
     public void setExperiencia(int exp) {
-        Experiencia=exp;
+        experiencia=exp;
     }
     public int getExperiencia() {
-        return Experiencia;
+        return experiencia;
     }
 }
