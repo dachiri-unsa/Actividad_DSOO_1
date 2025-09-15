d9bf2f90e0c0bab118c27225ae1d5c33d2c5a001
Xdaniel <dachiri@unsa.edu.pe>
2025-09-14 20:44:59 -0500

    Creacion de clases mejores XD

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,8 +1,28 @@
+import java.util.*;
 public class Ejercicio {
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("Bienvenido a la Sistema de vehiculos de transporte");
         while (true){
-            System.out.println("Bienvenido a la Sistema de vehiculos de transporte");
+            System.out.println("Seleccione una opcion:\n1) Añadir vehiculo\n2) Añadir chofer\n3) Escoger viaje");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Añadiendo vehiculo...");
+                    break;
+                case 2:
+                    System.out.println("Añadiendo chofer...");
+                    break;
+                case 3:
+                    System.out.println("Calculando viaje...");
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
             break;
         }
+        sc.close();
     }
 }
