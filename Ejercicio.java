fa9db428c5c12af1c8b3f4717c035821fbd57bf7
Xdaniel <dachiri@unsa.edu.pe>
2025-09-14 20:23:16 -0500

    solucion? 2 XD

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,14 +1,9 @@
 public class Ejercicio {
     public static void main(String[]args){
-<<<<<<< HEAD
         System.out.println("Esta es una prueba");
         System.out.println("RUBIAS");
         System.out.println("hola");
-=======
-        while (true){
-            System.out.println("Bienvenido a Sistema de viajes");
-            break;
-        }
->>>>>>> 3454619 (Bienvenida)
+        System.out.println("XD");
+        System.out.println("Solucion?");
     }
 }
