fcc6decf010c7092e3ca841f406f3742b797ef34
Leo_Bv <lcavero@unsa.edu.pe>
2025-09-14 22:55:06 -0500

    viaje1

--- Viaje.java
+++ Viaje.java
@@ -1,3 +1,15 @@
 public class Viaje {
     private int duración,costo;
+    public void setDuracion(int dur) {
+        duración=dur;
+    }
+    public int getDuracion() {
+        return duración;
+    }
+    public void setCosto(int cost) {
+        costo=cost;
+    }
+    public int getCosto() {
+        return costo;
+    }
 }
